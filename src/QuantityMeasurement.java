d5ef958da2e7aa82ec9509d447f4aa31de2b1d7a
noothanprem <rutujatikhile@gmail.com>
2020-02-26 17:49:22 +0530

    Added: testcase 1.2

--- src/QuantityMeasurement.java
+++ src/QuantityMeasurement.java
@@ -1,12 +1,14 @@
-import java.util.Scanner;
 
 public class QuantityMeasurement {
-    Scanner s = new Scanner(System.in);
-    public static void main(String[] args) {
-        System.out.println("Welcome");
-    }
 
-    public int feetToInch(int feet) {
-        return feet * 12;
+    public Object feet(Object feet) {
+        if (feet instanceof Integer)
+        {
+            return (int) feet;
+        }
+        else
+        {
+            return "Null value";
+        }
     }
 }
